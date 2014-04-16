a91f0f21df3e2c54e13bd95a673488c7d9d87dc9
Adar Dembo <adar@cloudera.com>
2014-04-15 21:05:44 -0700

    reformatted

--- src/com/muttbyte/tcpproxy/ProxyServer.java
+++ src/com/muttbyte/tcpproxy/ProxyServer.java
@@ -6,70 +6,71 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ProxyServer {
-    public final static int NUM_THREADS = 3;
-    public static final int DEFAULT_BACKLOG = 1000;
+  public static final int NUM_THREADS = 3;
+  public static final int DEFAULT_BACKLOG = 1000;
 
-    public static void main(String[] argv) {
-        int inPort = Integer.parseInt(argv[0]);
-        String outAddr = argv[1];
-        int outPort = Integer.parseInt(argv[2]);
+  public static void main(String[] argv) {
 
-        System.out.println("Proxy port " + inPort + " to addr " + outAddr + " port " + outPort);
-        ServerSocket servSock = null;
+    int inPort = Integer.parseInt(argv[0]);
+    String outAddr = argv[1];
+    int outPort = Integer.parseInt(argv[2]);
+
+    System.out.println("Proxy port " + inPort + " to addr " + outAddr + " port " + outPort);
+    ServerSocket servSock = null;
+    try {
+      servSock = new ServerSocket(inPort, DEFAULT_BACKLOG);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+      SingleProxy singleProxy = new SingleProxy(servSock, outAddr, outPort);
+      singleProxy.run();
+    }
+
+    System.out.println("Done.");
+  }
+
+  public static class SingleProxy extends Thread {
+
+    ServerSocket servSock;
+    String outAddr;
+    int outPort;
+
+    public SingleProxy(ServerSocket servSock, String outAddr, int outPort) {
+      this.servSock = servSock;
+      this.outAddr = outAddr;
+      this.outPort = outPort;
+    }
+
+    @Override
+    public void run() {
+
+      while (true) {
+
         try {
-            servSock = new ServerSocket(inPort, DEFAULT_BACKLOG);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        for (int i = 0; i < NUM_THREADS; i++) {
-            SingleProxy singleProxy = new SingleProxy(servSock, outAddr, outPort);
-            singleProxy.run();
-        }
+          Socket serverSocket = servSock.accept();
+          InetAddress outInetAddr = InetAddress.getByName(outAddr);
+          Socket cliSock = new Socket(outInetAddr, outPort);
 
-        System.out.println("Done.");
+          Object lock = new Object();
+          StreamCopyThread sToC = new StreamCopyThread(serverSocket, cliSock, lock);
+          StreamCopyThread cToS = new StreamCopyThread(cliSock, serverSocket, lock);
+          sToC.setPeer(cToS);
+          cToS.setPeer(sToC);
+          // synchronized (lock) {
+            // connections.addElement(cToS);
+            // connections.addElement(sToC);
+            sToC.start();
+            cToS.start();
+            // }
+
+        } catch (Exception exc) {
+          System.out.println(exc.toString());
+          exc.printStackTrace();
+        }
+      }
     }
-
-    public static class SingleProxy extends Thread {
-
-        ServerSocket servSock;
-        String outAddr;
-        int outPort;
-
-        public SingleProxy(ServerSocket servSock, String outAddr, int outPort) {
-            this.servSock = servSock;
-            this.outAddr = outAddr;
-            this.outPort = outPort;
-        }
-
-        @Override
-        public void run() {
-
-            while (true) {
-
-                try {
-
-                    Socket serverSocket = servSock.accept();
-                    InetAddress outInetAddr = InetAddress.getByName(outAddr);
-                    Socket cliSock = new Socket(outInetAddr, outPort);
-
-                    Object lock = new Object();
-                    StreamCopyThread sToC = new StreamCopyThread(serverSocket, cliSock, lock);
-                    StreamCopyThread cToS = new StreamCopyThread(cliSock, serverSocket, lock);
-                    sToC.setPeer(cToS);
-                    cToS.setPeer(sToC);
-                    // synchronized (lock) {
-                    // connections.addElement(cToS);
-                    // connections.addElement(sToC);
-                    sToC.start();
-                    cToS.start();
-                    // }
-
-                } catch (Exception exc) {
-                    System.out.println(exc.toString());
-                    exc.printStackTrace();
-                }
-            }
-        }
-    }
+  }
 }
